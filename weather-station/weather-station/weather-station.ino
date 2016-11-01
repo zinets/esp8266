5f4cff368d4a45a95da217c45dd31816510bfee1
zinets <zinetz.victor@gmail.com>
2016-11-01 22:09:22 +0200

    ++ update time by udp

--- weather-station/weather-station/weather-station.ino
+++ weather-station/weather-station/weather-station.ino
@@ -2,6 +2,63 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include "WiFiManager.h"
+#include <WiFiUdp.h>
+
+#include <Wire.h>
+#include "RTClib.h"
+
+// UDP - получение точного времени
+RTC_DS1307 rtc;
+const int timeZone = 3;               // Central European Time
+const char* timerServerDNSName = "0.europe.pool.ntp.org";
+IPAddress timeServer;
+
+WiFiUDP Udp;
+const unsigned int localPort = 8888;  // local port to listen for UDP packets
+const int NTP_PACKET_SIZE = 48;       // NTP time is in the first 48 bytes of message
+byte packetBuffer[NTP_PACKET_SIZE];
+
+void sendNTPpacket(IPAddress &address) {
+  memset(packetBuffer, 0, NTP_PACKET_SIZE);
+  packetBuffer[0] = 0b11100011; // LI, Version, Mode
+  packetBuffer[1] = 0;          // Stratum, or type of clock
+  packetBuffer[2] = 6;          // Polling Interval
+  packetBuffer[3] = 0xEC;       // Peer Clock Precision
+  // 8 bytes of zero for Root Delay & Root Dispersion
+  packetBuffer[12]  = 49;
+  packetBuffer[13]  = 0x4E;
+  packetBuffer[14]  = 49;
+  packetBuffer[15]  = 52;
+
+  Udp.beginPacket(address, 123);
+  Udp.write(packetBuffer, NTP_PACKET_SIZE);
+  Udp.endPacket();
+}
+
+time_t getNtpTime() {
+  while (Udp.parsePacket() > 0) ; // discard any previously received packets
+  Serial.println("Transmit NTP request");
+  sendNTPpacket(timeServer);
+  uint32_t beginWait = millis();
+  while (millis() - beginWait < 1500) {
+    int size = Udp.parsePacket();
+    if (size >= NTP_PACKET_SIZE) {
+      Serial.println("Receive NTP response");
+      Udp.read(packetBuffer, NTP_PACKET_SIZE);
+      unsigned long secsSince1900;
+      // convert four bytes starting at location 40 to a long integer
+      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
+      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
+      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
+      secsSince1900 |= (unsigned long)packetBuffer[43];
+      #define SECS_PER_HOUR 60
+      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
+    }
+  }
+
+  Serial.println("No NTP response");
+  return 0;
+}
 
 void configModeCallback (WiFiManager *myWiFiManager) {
   Serial.println("Entered config mode");
@@ -13,6 +70,8 @@ void configModeCallback (WiFiManager *myWiFiManager) {
 void setup() {
   Serial.begin(115200);
 
+  Wire.begin(0, 2);
+  
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
@@ -35,6 +94,44 @@ void setup() {
 
   //if you get here you have connected to the WiFi
   Serial.println("WiFi connected.");
+
+  Serial.println("Setup time..");
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  WiFi.hostByName(timerServerDNSName, timeServer);
+  Serial.print("Time server IP: ");
+  Serial.println(timeServer.toString());
+
+  Udp.begin(localPort);
+  Serial.print("UDP local port: ");
+  Serial.println(Udp.localPort());
+
+  time_t t = getNtpTime();
+  if (t != 0) {
+    Serial.println(t, DEC);
+
+    rtc.adjust(DateTime(t));
+    DateTime now = rtc.now();
+
+    Serial.println(now.unixtime(), DEC);
+
+    Serial.println("Board time:");
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+  }
 }
 
 void loop() {
