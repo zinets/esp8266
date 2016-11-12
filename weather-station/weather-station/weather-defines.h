ba900e8b4a8a534d58435f4739b4b6b9e07bfc64
zinets <zinetz.victor@gmail.com>
2016-11-12 19:19:46 +0200

    ++ cosmetic

--- weather-station/weather-station/weather-defines.h
+++ weather-station/weather-station/weather-defines.h
@@ -1,17 +1,5 @@
 #pragma once
 
-// typedef enum WeatherCondition {
-//   WeatherConditionClear,
-//   WeatherConditionRain,
-//   WeatherConditionSnow,
-//   WeatherConditionSleet,
-//   WeatherConditionWind,
-//   WeatherConditionFog,
-//   WeatherConditionCloudy,
-//   WeatherConditionPartlyCloudy,
-//   WeatherConditionIndoor,
-// } WeatherCondition;
-
 typedef struct WeatherState {
   // WeatherCondition condition;
   bool ready;
